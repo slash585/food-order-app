d76b2bdd1a6457acd9f7c3e5c4fad907d2bd3ca2
slash585 <slsh585@gmail.com>
2022-03-09 21:35:39 +0300

    Add modal and manage cart & modal state

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import { Fragment } from "react"
 import Header from "./components/Layout/Header"
 import "./App.css"
 import Meals from "./components/Meals/Meals"
+import ShoppingCart from "./components/Card/ShoppingCart"
+import { useState } from "react"
 
 function App() {
+  const [cartVisibility, setCartVisibility] = useState(false)
+
+  const onShowCartHandler = () => {
+    setCartVisibility(true)
+  }
+
+  const onHideCartHandler = () => {
+    setCartVisibility(false)
+  }
+
   return (
     <Fragment>
-      <Header />
+      {cartVisibility && <ShoppingCart onHideCart={onHideCartHandler} />}
+      <Header onShowCart={onShowCartHandler} />
       <main>
         <Meals />
       </main>
--- src/components/Card/ShoppingCart.js
+++ src/components/Card/ShoppingCart.js
@@ -1,12 +1,13 @@
 import React from "react"
+import Modal from "../UI/Modal"
 import classes from "./ShoppingCart.module.css"
 
-function ShoppingCart() {
+function ShoppingCart(props) {
   const shoppingCartItem = [
     { id: "c1", name: "Sushi", amount: 2, price: 12.99 },
   ]
   return (
-    <div>
+    <Modal onHideCart={props.onHideCart}>
       <ul className={classes["cart-items"]}>
         {shoppingCartItem.map((item) => (
           <li key={item.id}>{item.name}</li>
@@ -17,10 +18,12 @@ function ShoppingCart() {
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button-alt"]}>Close</button>
+        <button onClick={props.onHideCart} className={classes["button-alt"]}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   )
 }
 
